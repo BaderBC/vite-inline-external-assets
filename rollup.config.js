1187fcb6fff7e801693bcd6cf065b8fb7c64d971
Bartłomiej Strama <kontakt@bstrama.com>
2024-10-30 18:01:02 +0100

    no external libs

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,21 @@
+import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import inject from '@rollup/plugin-inject';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/index.cjs',
+      //file: 'dist/index.cjs',
+      dir: 'dist',
       format: 'cjs',
     },
     {
-      file: 'dist/index.js',
+      //file: 'dist/index.js',
+      dir: 'dist',
       format: 'esm',
     },
   ],
@@ -18,6 +23,8 @@ export default {
     resolve(),
     commonjs(),
     typescript(),
+    json(),
+    terser(),
   ],
   external: [],
 };
